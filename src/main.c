152fc28d6d716e3c3eb1145dba0368168020e1e5
Samuel Wahba <samuel.wahba@epfl.ch>
2024-03-22 13:57:40 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* hello world example: calling functions from a static library */
+
+
+#include <zephyr/kernel.h>
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World!\n");
+	return 0;
+}
