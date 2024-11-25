78988f448bb37f15e6812cf832a74212ca267918
Yann Gaspoz <yann@konzeptplus.ch>
2024-11-25 23:23:59 +0100

    added test code and fixed client, also removed dynamic casts

--- Client.cpp
+++ Client.cpp
@@ -1,5 +1,7 @@
 #include "Client.h"
 #include <cstring>
+#include <iostream>
+#include <vector>
 
 // ThreeFloatModule.cpp
 ThreeFloatModule::ThreeFloatModule() : value1(0.0f), value2(0.0f), value3(0.0f) {}
@@ -37,36 +39,42 @@ void OneFloatModule::unpack_message(const char *buffer) {
     std::memcpy(&value, &buffer[0], sizeof(float));
 }
 
-void Manager::add_module(Module *module) {
-    modules.push_back(module);
+// Manager.cpp
+void Manager::set_module_configuration(char identifier, const std::vector<Module *> &modules) {
+    module_configurations[identifier] = modules;
 }
 
 void Manager::generate_combined_message(char identifier, char *combined_buffer) {
     combined_buffer[0] = identifier;
     int offset = 1;
-    for (auto &module : modules) {
+    for (auto &module : module_configurations[identifier]) {
         char module_buffer[12];
         module->generate_message(module_buffer);
-        std::memcpy(&combined_buffer[offset], module_buffer, 12);
-        offset += 12;
+        int module_size = (module->get_type() == Module::Type::ThreeFloat) ? 12 : 4;
+        std::memcpy(&combined_buffer[offset], module_buffer, module_size);
+        offset += module_size;
     }
 }
 
 void Manager::unpack_combined_message(const char *combined_buffer, char &identifier, std::vector<std::vector<float>> &values) {
     identifier = combined_buffer[0];
     int offset = 1;
-    for (auto &module : modules) {
+    for (auto &module : module_configurations[identifier]) {
         std::vector<float> module_values;
-        if (dynamic_cast<ThreeFloatModule*>(module)) {
+        if (module->get_type() == Module::Type::ThreeFloat) {
             float value1, value2, value3;
             module->unpack_message(&combined_buffer[offset]);
+            std::memcpy(&value1, &combined_buffer[offset], sizeof(float));
+            std::memcpy(&value2, &combined_buffer[offset + 4], sizeof(float));
+            std::memcpy(&value3, &combined_buffer[offset + 8], sizeof(float));
             module_values.push_back(value1);
             module_values.push_back(value2);
             module_values.push_back(value3);
             offset += 12;
-        } else if (dynamic_cast<OneFloatModule*>(module)) {
+        } else if (module->get_type() == Module::Type::OneFloat) {
             float value;
             module->unpack_message(&combined_buffer[offset]);
+            std::memcpy(&value, &combined_buffer[offset], sizeof(float));
             module_values.push_back(value);
             offset += 4;
         }
--- Client.h
+++ Client.h
@@ -4,11 +4,15 @@
 #include <vector>
 #include <tuple>
 #include <cstring>
+#include <unordered_map>
 
 class Module {
 public:
+    enum class Type { ThreeFloat, OneFloat };
     virtual void generate_message(char *buffer) = 0;
     virtual void unpack_message(const char *buffer) = 0;
+    virtual Type get_type() const = 0;
+    virtual ~Module() = default;
 };
 
 class ThreeFloatModule : public Module {
@@ -17,18 +21,19 @@ public:
     void set_values(float value1, float value2, float value3);
     void generate_message(char *buffer) override;
     void unpack_message(const char *buffer) override;
+    Type get_type() const override { return Type::ThreeFloat; }
 
 private:
     float value1, value2, value3;
 };
 
-// OneFloatModule.h
 class OneFloatModule : public Module {
 public:
     OneFloatModule();
     void set_value(float value);
     void generate_message(char *buffer) override;
     void unpack_message(const char *buffer) override;
+    Type get_type() const override { return Type::OneFloat; }
 
 private:
     float value;
@@ -36,12 +41,12 @@ private:
 
 class Manager {
 public:
-    void add_module(Module *module);
+    void set_module_configuration(char identifier, const std::vector<Module *> &modules);
     void generate_combined_message(char identifier, char *combined_buffer);
     void unpack_combined_message(const char *combined_buffer, char &identifier, std::vector<std::vector<float>> &values);
 
 private:
-    std::vector<Module *> modules;
+    std::unordered_map<char, std::vector<Module *>> module_configurations;
 };
 
 #endif // CLIENT_H
