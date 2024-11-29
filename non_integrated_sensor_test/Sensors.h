58876f5c04e0556750d86e3057297e003904d5ab
AlexOutis <alexaist4@gmail.com>
2024-11-29 11:16:46 +0100

    corrected sensors.h copy of .ino

--- non_integrated_sensor_test/Sensors.h
+++ non_integrated_sensor_test/Sensors.h
@@ -1,48 +1,125 @@
-#include "Sensors.h"
+#ifndef SENSORS_H
+#define SENSORS_H
 
-TripleBNO055 bno(0x28, 0x29, 0x29, &Wire, &Wire2);
-TripleBMP581 bmp(0x46, 0x47, 0x46, &Wire, &Wire2);
+#include <Arduino.h>
+#include <Wire.h>
+#include <SparkFun_BMP581_Arduino_Library.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+// Structure to hold BNO055 sensor data
+struct BNO055Data
+{
+    imu::Vector<3> orientation;
+    imu::Vector<3> gyroscope;
+    imu::Vector<3> acceleration;
+};
 
-const uint8_t bno_data_sz = 6;
-const uint8_t bmp_data_sz = 2;
-std::array<float, bno_data_sz> bno_data;
-std::array<float, bmp_data_sz> bmp_data;
+class Sensor
+{
+public:
+    virtual const bool setup() = 0;
+    virtual void calibrate() = 0;
+    virtual const float get_outlier_threshold() const { return outlier_threshold;}
+    virtual void set_outlier_threshold(float _outlier_threshold) { outlier_threshold = _outlier_threshold;}
+protected:
+    const float calculateMedian(float a, float b, float c) const;
+    virtual const float combineData(float m1, float m2, float m3) const;
+    float outlier_threshold;
+};
+
+// BNO055 Sensor Class
+class BNO055Sensor : public Sensor
+{
+public:
+    BNO055Sensor(uint8_t address, TwoWire *wire, uint8_t delay_time=10, uint8_t calibration_threshold=0, float outlier_threshold = 2);
+
+    const bool setup() override;
+    void calibrate() override;
+
+    const bool updateCalStatus();
+    void displayCalStatus() const;
+
+    const std::array<float, 6> readData();
+
+private:
+    uint8_t delay_time;
+    uint8_t calibration_threshold;
+    float outlier_threshold = 2;
+
+    Adafruit_BNO055 bno;
+    uint8_t system_cal_status;
+    uint8_t gyro_cal_status;
+    uint8_t accel_cal_status;
+    uint8_t mag_cal_status;
+};
 
 
-void initWire() {
-    Wire.begin();
-    Wire.setSCL(16);
-    Wire.setSDA(17);
-    Wire.setClock(400000);
+class TripleBNO055 : public Sensor {
+public:
+    TripleBNO055(uint8_t address1, uint8_t address2, uint8_t address3, TwoWire *wire, TwoWire *wire2, uint8_t delay_time=10, uint8_t calibration_threshold=0, float outlier_threshold = 2);
 
-    Wire2.begin();
-    Wire2.setSCL(24);
-    Wire2.setSDA(25);
-    Wire2.setClock(400000);
-}
+    const bool setup() override;
+    void calibrate() override;
+    void displayCalStatus() const;
+    void displayStatus() const;
 
-void setup() {
-    Serial.begin(115200);
-    Serial.println("Initializing serial communication");
-    initWire();
+    // Method to read data from all three sensors
+    const std::array<float, 6> read_data();
+    void print_data(const std::array<float, 6>&) const;
+    BNO055Sensor sensor1;
+    BNO055Sensor sensor2;
+    BNO055Sensor sensor3;
 
-    bool bno_status = bno.setup();
-    bno.displayStatus();
-    Serial.println("Finished initializing BNO");
+private:
+ 
+};
+// BMP581 Sensor Class
+struct BMPData{
+    float pressure;
+    float temperature;
+};
+// BMP581Sensor Class
+class BMP581Sensor : public Sensor {
+public:
+    BMP581Sensor(uint8_t address, TwoWire *wire, uint8_t delay_time=20, float outlier_threshold = 2);
 
-    bool bmp_status = bmp.setup();
-    bmp.displayStatus();
-    Serial.println("Finished initializing BMP");
+    const bool setup() override;
+    void calibrate() override;
+    const BMPData readData(); // Removed 'const'
+    const float readPressure() const;
+    const float readTemperature() const;
+    void enableFilteringAndOversampling();
 
-    bno.calibrate();
-    Serial.println("Finished calibrating BNO");
+private:
+    BMP581 bmp;
+    uint8_t delay_time;
+    float outlier_threshold = 2;
 
-    bmp.calibrate();
-    Serial.println("Finished calibrating BMP");
+    uint8_t i2c_address;
+    TwoWire *wire;
+};
 
-}
 
-void loop() {
-    bno_data = bno.read_data();(bmp_data);
-    Serial.println("");
-}
+class TripleBMP581 : public Sensor {
+public:
+    TripleBMP581(uint8_t address1, uint8_t address2, uint8_t address3, TwoWire *wire, TwoWire *wire2, uint8_t delay_time = 20, float outlier_threshold = 2);
+
+    const bool setup() override;
+    void calibrate() override;
+    void displayCalStatus() const;
+    void displayStatus() const;
+    // Method to read data from all three sensors
+    const std::array<float, 2> read_data();
+    const float read_pressure();
+
+    void print_data(const std::array<float, 2>&) const;
+    BMP581Sensor sensor1;
+    BMP581Sensor sensor2;
+    BMP581Sensor sensor3;
+
+private:
+
+};
+
+#endif
