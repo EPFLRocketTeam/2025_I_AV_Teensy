c5b1ef5bd784b444d8009cccfdeae874813aa811
Daniel Roulin <daniel.roulin@epfl.ch>
2025-03-25 15:29:29 +0100

    Updated to use latest version of the com_client

--- main_code_teensy.ino
+++ main_code_teensy.ino
@@ -1,5 +1,9 @@
-// Nescessary includes for UART
-#include "com_client/inc/PacketID.h"
+// Necessary includes for UART
+// These unfortunately have to be included in the main file
+#include "com_client/inc/Vec3.h"
+#include "com_client/inc/State.h"
+#include "com_client/inc/SetpointSelection.h"
+#include "com_client/inc/Packets.h"
 #include "com_client/inc/Payload.h"
 #include "com_client/inc/UART.h"
 #include "com_client/inc/TeensyUART.h"
@@ -7,25 +11,19 @@
 #include "com_client/src/UART.cpp"
 #include "com_client/src/TeensyUART.cpp"
 
-// All of the types from control
-#include "inc/Vec3.h"
-#include "inc/State.h"
-#include "inc/SetpointSelection.h"
-
 // Arduino libraries
-#include <SD.h>
 #include <ArduinoJson.h>
+#include <SD.h>
 
 // C++ libraries
 #include <list>
 #include <memory>
 
-
 #define MAX_WIDTH 2000. // max throttle,
 #define MIN_WIDTH 1000. // min throttle,
 
 #define MAX_TILT 10.   // the maximum angle the drone can be asked to tilt
-#define LOOP_PERIOD 10 // ends up being closer to 14 in reality 
+#define LOOP_PERIOD 10 // ends up being closer to 14 in reality
 #define MAX_THRUST 19  // max thrust
 
 #define SD_FLUSH_PERIOD 1000 // ms
@@ -36,62 +34,59 @@ uint32_t lastSDWrite = 0;
 #define inner_offset 3
 
 // enum of the different control modes
-enum ControlMode {
+enum ControlMode
+{
     ATTITUDE,
     RATE,
     ALTITUDE,
     ALTITUDE_VEL,
 };
 
-struct RemoteInput {
+struct RemoteInput
+{
     State desired;
     SetpointSelection setpointSelection;
-    std::optional<double>   inline_thrust = std::nullopt;
+    std::optional<double> inline_thrust = std::nullopt;
     ControlMode controlMode;
     bool arm = false;
 };
 
-struct SensorData {
+struct SensorData
+{
     State state;
-    double attitude_count=0;
-    double rate_count=0;
-    double pressure_count=0;
+    double attitude_count = 0;
+    double rate_count = 0;
+    double pressure_count = 0;
 };
 
-struct RawOutput {
-    double d1; // in degrees
-    double d2; // in degrees
-    double avg_throttle; // from 0 to 1
+struct RawOutput
+{
+    double d1;            // in degrees
+    double d2;            // in degrees
+    double avg_throttle;  // from 0 to 1
     double throttle_diff; // from -1 to 1, top_throttle - bot_throttle
 };
 
-struct FlightNumber {
+struct FlightNumber
+{
     int session_number = 0;
     int flight_number = 0;
-  };
-
-struct ControlInput
-{
-    bool armed;
-    State desired_state;
-    State current_state;
-    SetpointSelection setpointSelection;
-    double inline_thrust;
 };
 
-struct ControlOutput{
+struct ControlOutput
+{
     double d1 = 0;
     double d2 = 0;
     double thrust = 0;
     double mz = 0;
 
-    ControlOutput toDegrees(){
-        return {d1*180/M_PI, d2*180/M_PI, thrust, mz};
+    ControlOutput toDegrees()
+    {
+        return {d1 * 180 / M_PI, d2 * 180 / M_PI, thrust, mz};
     }
 };
 
-ControlOutput received_control_output;
-bool control_output_received = false;
+RawOutput received_output;
 
 uint32_t timer = millis();
 
@@ -128,7 +123,7 @@ void loop()
 
     // armed state transitions
     if (!armed && remote_input.arm)
-    {   
+    {
         create_flight_file();
         timer = loop_start;
         Serial.print("Armed\n");
@@ -146,7 +141,9 @@ void loop()
     {
         unarmed_output();
 
-        ControlInput control_input = {false, ERROR_STATE, ERROR_STATE, ATTITUDE_CONTROL_SELECTION, 0.0};
+        ControlInputPacket control_input;
+        control_input.armed = false;
+        control_input.timestamp = millis();
         SendControlInput(control_input);
     }
     // armed behavior
@@ -154,17 +151,20 @@ void loop()
     {
         SensorData sensor_data = read_sensors();
 
-        ControlInput control_input = {true, remote_input.desired, sensor_data.state, remote_input.setpointSelection, remote_input.inline_thrust.value_or(0)};
+        ControlInputPacket control_input = {
+            true,
+            millis(),
+            remote_input.desired,
+            sensor_data.state,
+            remote_input.setpointSelection,
+            remote_input.inline_thrust.value_or(0)
+        };
         SendControlInput(control_input);
 
-        if (control_output_received)
-        {
-            RawOutput raw_out = to_raw(received_control_output.toDegrees());
-        
-            write_raw_outputs(raw_out);
+        // TODO: Check if sequence number has increased here
+        write_raw_outputs(received_output);
 
-            print_state(loop_start - timer, raw_out, sensor_data);
-        }
+        print_state(loop_start - timer, received_output, sensor_data);
 
         if (loop_start - lastSDWrite > SD_FLUSH_PERIOD)
         {
--- uart.ino
+++ uart.ino
@@ -2,44 +2,42 @@ TeensyUART uart_manager(Serial7, 115200);
 
 void ReceiveControlOutput(Payload &payload)
 {
-    ControlOutput output;
-    bool success = true;
-
-    success &= payload.ReadDouble(output.d1);
-    success &= payload.ReadDouble(output.d2);
-    success &= payload.ReadDouble(output.thrust);
-    success &= payload.ReadDouble(output.mz);
+    ControlOutputPacket output_packet;
+    bool success = payload.ReadControlOutputPacket(output_packet);
 
     if (!success)
     {
-        Serial.println("Error: Failed to read complete ControlOutput from payload!");
+        Serial.println("Error: Failed to read output packet from payload!");
         return;
     }
 
-    Serial.print("Received control output: d1=");
-    Serial.print(output.d1);
-    Serial.print(", d2=");
-    Serial.print(output.d2);
-    Serial.print(", thrust=");
-    Serial.print(output.thrust);
-    Serial.print(", mz=");
-    Serial.println(output.mz);
+    // TODO: Average this value
+    // TODO: Log this to SD card
+    double round_trip_time = millis() - output_packet.timestamp;
+    Serial.print("Round trip time: ");
+    Serial.println(round_trip_time);
 
-    received_control_output = output;
-    control_output_received = true;
+    // Serial.print("Received control output: d1=");
+    // Serial.print(output.d1);
+    // Serial.print(", d2=");
+    // Serial.print(output.d2);
+    // Serial.print(", thrust=");
+    // Serial.print(output.thrust);
+    // Serial.print(", mz=");
+    // Serial.println(output.mz);
+
+    received_output = {
+        output_packet.d1,
+        output_packet.d2,
+        output_packet.avg_throttle,
+        output_packet.throttle_diff
+    };
 }
 
-bool SendControlInput(const ControlInput &input)
+bool SendControlInput(const ControlInputPacket &input)
 {
     Payload payload;
-    bool success = true;
-
-    success &= payload.WriteBool(input.armed);
-    success &= WriteState(payload, input.desired_state);
-    success &= WriteState(payload, input.current_state);
-    success &= WriteSetpointSelection(payload, input.setpointSelection);
-    success &= payload.WriteDouble(input.inline_thrust);
-    
+    bool success = payload.WriteControlInputPacket(input);
     if (!success)
     {
         Serial.println("Error: Failed to write control input to payload");
@@ -47,7 +45,6 @@ bool SendControlInput(const ControlInput &input)
     }
 
     success = uart_manager.SendUARTPacket((int)PacketId::ControlInput, payload);
-
     if (!success)
     {
         Serial.println("Error: Failed to send control input");
@@ -59,88 +56,6 @@ bool SendControlInput(const ControlInput &input)
     Serial.print(", thrust=");
     Serial.print(input.inline_thrust);
     Serial.println(", rest_of_the_payload=TODO");
-    
-    return true;
-}
-
-bool WriteSetpointSelection(Payload &payload, const SetpointSelection &setpoint)
-{
-    // Pack all 12 boolean values into 2 bytes to minimize payload size
-    uint8_t buffer[2] = {0, 0};
-
-    // Pack posSPActive (3 bits)
-    if (setpoint.posSPActive[0])
-        buffer[0] |= (1 << 0);
-    if (setpoint.posSPActive[1])
-        buffer[0] |= (1 << 1);
-    if (setpoint.posSPActive[2])
-        buffer[0] |= (1 << 2);
-
-    // Pack velSPActive (3 bits)
-    if (setpoint.velSPActive[0])
-        buffer[0] |= (1 << 3);
-    if (setpoint.velSPActive[1])
-        buffer[0] |= (1 << 4);
-    if (setpoint.velSPActive[2])
-        buffer[0] |= (1 << 5);
-
-    // Pack attSPActive (3 bits across byte boundary)
-    if (setpoint.attSPActive[0])
-        buffer[0] |= (1 << 6);
-    if (setpoint.attSPActive[1])
-        buffer[0] |= (1 << 7);
-    if (setpoint.attSPActive[2])
-        buffer[1] |= (1 << 0);
-
-    // Pack rateSPActive (3 bits)
-    if (setpoint.rateSPActive[0])
-        buffer[1] |= (1 << 1);
-    if (setpoint.rateSPActive[1])
-        buffer[1] |= (1 << 2);
-    if (setpoint.rateSPActive[2])
-        buffer[1] |= (1 << 3);
-
-    // Write the packed bytes to the payload
-    bool success = payload.WriteBytes(buffer, sizeof(buffer));
-
-    if (!success)
-    {
-        Serial.println("Error: Failed to write SetpointSelection to payload");
-        return false;
-    }
-
-    return true;
-}
-
-bool WriteState(Payload &payload, const State &state)
-{
-    bool success = true;
-    success &= WriteVec3(payload, state.pos);
-    success &= WriteVec3(payload, state.vel);
-    success &= WriteVec3(payload, state.att);
-    success &= WriteVec3(payload, state.rate);
-
-    if (!success)
-    {
-        Serial.println("Error: Failed to write state to payload");
-        return false;
-    }
-
-    return true;
-}
-
-bool WriteVec3(Payload &payload, Vec3 vec)
-{
-    bool success = true;
-    success &= payload.WriteDouble(vec.x);
-    success &= payload.WriteDouble(vec.y);
-    success &= payload.WriteDouble(vec.z);
-
-    if (!success)
-    {
-        Serial.println("Error: Failed to write Vec3 to payload");
-        return false;
-    }
 
     return true;
 }
